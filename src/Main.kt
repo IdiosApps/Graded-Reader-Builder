b2a2f07ea0ecabc3eb01e8d7ceb93368f90258ea
James Clark <james.sw.clark@gmail.com>
2017-07-15 16:37:34 +0100

    - Added code to prepare vocab (split into chinese/english, store for later)
    - Added code to automate vocab sub/super-scripts.
    
    - Started considering how to handle vocab footers. Have some code, but will likely need to go into page-counting methods. A cheap fix for now (and a handy feature) would be to give a vocab list at the back of the book. [todo]
    
    - Add pinyin compatability for vocab [todo]

--- src/Main.kt
+++ src/Main.kt
@@ -1,6 +1,12 @@
 import java.io.File
 import java.io.PrintWriter
 import java.util.*
+import java.nio.file.Files
+import java.nio.charset.Charset
+import java.nio.file.Paths
+import java.nio.charset.StandardCharsets
+
+
 
 /**
  * Created by james-clark-5 (Idios) on 12/06/17.
@@ -25,11 +31,24 @@ import java.util.*
 
 
 fun main(args: Array<String>) {
+    // load input/output files
     val inputStoryFilename: String = "res/inputStory"
     val inputVocabFilename: String = "res/inputVocab"
     val inputHeaderFilename: String = "res/inputHeader"
     val outputStoryFilename: String = "res/outputStory.tex"
-    val outputStoryWriter = PrintWriter(outputStoryFilename, "UTF-8") // create writer for output
+
+    // Create arrays to store vocab info
+    var vocabArray: ArrayList<String> = ArrayList<String>()
+    var vocabComponentArray: ArrayList<ArrayList<String>> = ArrayList<ArrayList<String>>()
+
+    // create writer for output
+    val outputStoryWriter = PrintWriter(outputStoryFilename, "UTF-8")
+
+    // PREPARATION of vocab
+    vocabToArray(inputVocabFilename, vocabArray, vocabComponentArray) // take the vocab text and split into components
+
+
+    // create output from input files
     writeTexHeader(outputStoryWriter, inputHeaderFilename)
     writeTexStory(outputStoryWriter, inputStoryFilename)
     // addVocabFooters()
@@ -37,6 +56,83 @@ fun main(args: Array<String>) {
     outputStoryWriter.println("\\end{document}") // end the TeX document
     outputStoryWriter.close()
 
+    addVocabSubsripts(vocabComponentArray, outputStoryFilename, outputStoryWriter)
+
+}
+
+fun addVocabFooters(vocabComponentArray: ArrayList<ArrayList<String>>, outputStoryFilename: String, outputStoryWriter: PrintWriter ){
+    // open the output file, with header and story
+    val outputStoryFile: File = File(outputStoryFilename)
+    val scan: Scanner = Scanner(outputStoryFile)
+    // for each word in our vocab list, find the first entry and add a footer
+    // n.b. add superscripts later
+    vocabComponentArray.forEachIndexed { index, currentSubStringWordType ->
+        while(scan.hasNextLine()) {
+            val line: String = scan.nextLine()
+            if (line.contains((vocabComponentArray[index][0]))) {
+                // add footer text                  TODO figure out how to approach this
+//                \lfoot{
+//                    x. 对她说 (\pinyin{dui4ta1shuo1}) said to her\\
+//                    x. 啊 (\pinyin{a1}) who knows..\\
+//                    x. 聪明 (\pinyin{cong1ming}) intelligent\\
+//                }
+//
+
+                outputStoryWriter.println("\\setstretch{1.5}")
+
+                break
+            }
+        }
+    }
+}
+
+fun addVocabSubsripts(vocabComponentArray: ArrayList<ArrayList<String>>, outputStoryFilename: String, outputStoryWriter: PrintWriter ){
+    // open the output file, with header and story
+    val outputStoryFile: File = File(outputStoryFilename)
+    val scan: Scanner = Scanner(outputStoryFile)
+
+    // prepare to replace content in outputStoryFile
+    val path = Paths.get(outputStoryFilename)
+    val charset = StandardCharsets.UTF_8
+    var content = String(Files.readAllBytes(path), charset)
+
+    // replace vocab words w/ the same words PLUS sub/superscript info
+    vocabComponentArray.forEachIndexed { index, currentSubStringWordType ->
+        content = content.replace(vocabComponentArray[index][0].toRegex(), vocabComponentArray[index][0] + "\\\\textsubscript{" + (index+1) + "}")
+    }
+    Files.write(path, content.toByteArray(charset))
+}
+
+fun vocabToArray(inputVocabFilename: String, vocabArray: ArrayList<String>, vocabComponentArray: ArrayList<ArrayList<String>>){
+    val inputVocabFile: File = File(inputVocabFilename) // get file ready
+    val scan: Scanner = Scanner(inputVocabFile)
+    var vocabLineCount: Int = 0
+    var tmpComponentArrayList:ArrayList<String> = ArrayList<String>()
+    while(scan.hasNextLine()) {
+        val line: String = scan.nextLine() // read all lines
+
+
+        // split each entry into 2 components: Chinese, English (to find the Chinese in the text)
+        var componentList: List<String> = line.split(" ")
+        var splitIndex: Int = line.indexOf("|")
+        var chineseSplit: String = line.substring(0, splitIndex)
+        var englishSplit: String = line.substring(splitIndex+1, line.length)
+        var ArrayListInitialiser: ArrayList<String> = ArrayList<String>(Collections.singletonList(""))
+
+        // store the whole entry (to go directly into the footer)
+        vocabArray.add(chineseSplit + ": " + englishSplit)
+
+        // store the individual Chinese and English components
+        vocabComponentArray.add(ArrayListInitialiser)
+        vocabComponentArray[vocabLineCount].add(chineseSplit)
+        vocabComponentArray[vocabLineCount].add(englishSplit)
+        vocabComponentArray[vocabLineCount].remove(vocabComponentArray[vocabLineCount][0]) // "uninitilaise" ArrayList empty entry
+
+        vocabLineCount+=1
+    }
+    scan.close()
+    println("vocabArray: " + vocabArray)
+    println("vocabComponentArray: " + vocabComponentArray)
 }
 
 fun writeTexHeader(outputStoryWriter: PrintWriter, inputHeaderFilename: String){
@@ -72,14 +168,5 @@ fun writeTexStory(outputStoryWriter: PrintWriter, inputStoryFilename: String){
     scan.close()
 }
 
-fun addVocabFooters(){
-    // split the InputVocabFile by lines (->array list) and find first part (by space delimiting) of given array entry
-    // find the first instance of a given word from the vocab list in the output story
-    // for that page, add a footer with the full information of that array entry
 
-}
-
-fun addVocabSuperscripts(){
-    // for each entry in the vocab list, go through the outputStoryFile and add superscripts with that entry's index+1
-}
 
