87cc7d8ec7beb4e50ffdf6f5c29c98dde1366305
James Clark <idiosapps@gmail.com>
2019-01-05 12:24:01 +0000

    - Move new GUI fxml to /src/ - Attach Controller to fxml

--- src/GUIController.kt
+++ src/GUIController.kt
@@ -7,84 +7,31 @@ import javafx.scene.control.Label
 
 class GUIController {
 
-    @FXML
-    val build: JFXButton? = null
-
-    @FXML
-    val myComboBoxL1: JFXComboBox<Label> = JFXComboBox()
-    @FXML
-    val myComboBoxL2: JFXComboBox<Label> = JFXComboBox()
-
-    @FXML
-    val errorLog: JFXTextArea = JFXTextArea()
-
-    @FXML
-    val info: JFXTextArea = JFXTextArea()
-
-    @FXML
-    fun buildGR(event: ActionEvent) {
-        info.setPromptText("Pressed build. This is errorlog.")
-        info.setFocusTraversable(false); // set focus traversable false.
-    System.out.println("Button Action");
-        checkXeLaTeX()
+//    @FXML
+//    fun buildGR(event: ActionEvent) {
+//        info.setPromptText("Pressed build. This is errorlog.")
+//        info.setFocusTraversable(false); // set focus traversable false.
+//    System.out.println("Button Action");
+//        checkXeLaTeX()
 //        var argArray: ArrayList<String> = ArrayList<String>()
 //        Main.main(argArray)
-    }
+//    }
 
     @FXML
-    fun sendL1(event: ActionEvent) {
-    println("l1 pressed")
+    fun clickedBuildButton(event: ActionEvent) {
+        println("buildButton pressed")
     }
 
-    @FXML
-    fun sendL2(event: ActionEvent) {
-        println("l2 pressed")
-    }
-
-    fun setData() {
-        myComboBoxL1.getItems().add(Label("Java 1.8"))
-        myComboBoxL1.getItems().add(Label("Java 1.7"))
-        myComboBoxL1.getItems().add(Label("Java 1.6"))
-        myComboBoxL1.getItems().add(Label("Java 1.5"))
-
-        myComboBoxL1.setPromptText("Select Java Version")
-    }
-//    myComboBoxL1.getItems().clear()
-//
-//    myComboBoxL1.getItems().addAll(
-//    "jacob.smith@example.com",
-//    "isabella.johnson@example.com",
-//    "ethan.williams@example.com",
-//    "emma.jones@example.com",
-//    "michael.brown@example.com")
-//     }
-
-//    var options = FXCollections.observableArrayList(
-//            "English",
-//            "Chinese (Mandarin)",
-//            "Option 3"
-//    )
-//    myComboBoxL1.setItems(options)
-
-//    @FXML
-//    myComboBoxL1 = JFXComboBox(options)
-//
-//    myComboBoxL2 = JFXComboBox(options)
-
-
-    fun initialize() {
-        println("Label is null? ")
-        setData(); println("setData")
-//        val myComboBoxL1 = ComboBox(options)
-    }
-
-    private fun checkXeLaTeX(){
-        errorLog.setPromptText("Checking for xelatex support.")
-        var scanner: java.util.Scanner = java.util.Scanner(Runtime.getRuntime().exec("xelatex").getInputStream()) // .useDelimiter("\\A"
-        var line = scanner.next()
-        var hasXeTeX = (line.contains("This")) // "This is XeTeX, version ....."
-        if (hasXeTeX){errorLog.setPromptText("yay")}
-        else {errorLog.setPromptText("XeTeX not detected. &#10;Download XeTeX here: http://www.texts.io/support/0002/")}
-        errorLog.setFocusTraversable(false); // set focus traversable false.
-    }
+    fun initialize() {}
 }
+
+//    private fun checkXeLaTeX(){
+//        errorLog.setPromptText("Checking for xelatex support.")
+//        var scanner: java.util.Scanner = java.util.Scanner(Runtime.getRuntime().exec("xelatex").getInputStream()) // .useDelimiter("\\A"
+//        var line = scanner.next()
+//        var hasXeTeX = (line.contains("This")) // "This is XeTeX, version ....."
+//        if (hasXeTeX){errorLog.setPromptText("yay")}
+//        else {errorLog.setPromptText("XeTeX not detected. &#10;Download XeTeX here: http://www.texts.io/support/0002/")}
+//        errorLog.setFocusTraversable(false); // set focus traversable false.
+//    }
+//}
--- src/MainGUI.kt
+++ src/MainGUI.kt
@@ -1,11 +1,8 @@
-//import GUIController.GUIController
 import javafx.application.Application
-import javafx.collections.FXCollections
 import javafx.scene.Scene
 import javafx.stage.Stage
 import java.io.FileInputStream
 import javafx.fxml.FXMLLoader
-import javafx.scene.control.ComboBox
 import javafx.scene.layout.VBox
 
 // Made with the help of @danielt998's H2A + http://code.makery.ch/library/javafx-8-tutorial/part1/
@@ -15,36 +12,16 @@ class MainGUI: Application() {
     override fun start(givenStage: Stage) {
 
         val loader = FXMLLoader()
-        val fxmlDocPath = "src/GRB_SB_JFX.fxml"
+        val fxmlDocPath = "src/UI.fxml"
         val fxmlStream = FileInputStream(fxmlDocPath)
         val rootLayout = loader.load(fxmlStream) as VBox
         var myController = loader.getController<GUIController>()
         // Show the scene containing the root layout.
         val scene = Scene(rootLayout)
 
-
-        myController.setData()
-
-//        var myComboBoxL1 = ComboBox<String>()
-//        myComboBoxL1.getItems().addAll(
-//                "jacob.smith@example.com",
-//                "isabella.johnson@example.com",
-//                "ethan.williams@example.com",
-//                "emma.jones@example.com",
-//                "michael.brown@example.com")
-
-
         givenStage.setScene(scene)
         givenStage.show()
-
-
-
-//        GUIController.myComboBox.setItems(myComboBoxData);
-//        showPersonOverview();
     }
-
-
-
 }
 
 fun main(args: Array<String>) {
